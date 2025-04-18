f3c66dba0ec1c6e19ea1899a22078a628d2c51ee
emcarlos <carlosemc12@gmail.com>
2025-04-18 00:01:05 -0300

    feat: refactor App component to integrate React Query, routing, and UI providers for enhanced functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { FavoritesProvider } from "./contexts/FavoritesContext";
+import Landing from "./pages/PublicRoutes/Landing";
 
-function App() {
-  const [count, setCount] = useState(0)
+const queryClient = new QueryClient();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    {import.meta.env.DEV && (
+      <ReactQueryDevtools
+        initialIsOpen={false}
+        buttonPosition="bottom-left"
+      />
+    )}
+    <FavoritesProvider>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={<Landing />}
+            />
+            {/* <Route
+              path="/products"
+              element={<Products />}
+            />
+            <Route
+              path="/favorites"
+              element={<Favorites />}
+            /> */}
+            {/* <Route
+              path="/about"
+              element={<About />}
+            />
+            <Route
+              path="/contact"
+              element={<Contact />}
+            /> */}
+            {/* <Route
+              path="/signin"
+              element={<SignIn />}
+            />
+            <Route
+              path="/signup"
+              element={<SignUp />}
+            />
+            <Route
+              path="*"
+              element={<NotFound />}
+            /> */}
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </FavoritesProvider>
+  </QueryClientProvider>
+);
 
-export default App
+export default App;
