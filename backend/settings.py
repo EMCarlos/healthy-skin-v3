e9dca958d843a20734a41e9a8120489a7004f38d
emcarlos <carlosemc12@gmail.com>
2025-04-20 00:23:44 -0300

    fix: update ALLOWED_HOSTS

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'healthyskin.herokuapp.com', 'www.healthyskinaruba.com', 'healthyskinaruba.com', "healthy-skin-v3.vercel.app"]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'healthyskin.herokuapp.com', 'healthy-skin-v3-feb7e40fa621.herokuapp.com', 'www.healthyskinaruba.com', 'healthyskinaruba.com', "healthy-skin-v3.vercel.app"]
 
 
 # Application definition
