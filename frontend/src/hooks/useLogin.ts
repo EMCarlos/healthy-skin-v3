1b133be14f45fbeadde874277f065812d598df4a
emcarlos <carlosemc12@gmail.com>
2025-06-19 21:31:56 -0300

    fix: add setIsLogged to useGeneralStore in useLogin hook for state management

--- frontend/src/hooks/useLogin.ts
+++ frontend/src/hooks/useLogin.ts
@@ -5,7 +5,7 @@ import { useState } from "react";
 const useLogin = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
-  const { userLogged, setUserLogged } = useGeneralStore();
+  const { userLogged, setUserLogged, setIsLogged } = useGeneralStore();
 
   const login = async ({ email, password }: { email: string; password: string }) => {
     setLoading(true);
@@ -19,6 +19,7 @@ const useLogin = () => {
 
       const { data } = await axios.post("/api/users/login/", { username: email, password }, config);
 
+      setIsLogged(true);
       setUserLogged(data);
     } catch (err: any) {
       setError(err.response && err.response.data.detail ? err.response.data.detail : err.message);
