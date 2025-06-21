465f0d4c1f4804a7954ac8a41d33f6dbf4233726
emcarlos <carlosemc12@gmail.com>
2025-06-21 00:06:52 -0300

    refactor: change getUsers response from JsonResponse to Response for consistency

--- base/views/user_views.py
+++ base/views/user_views.py
@@ -5,6 +5,8 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from django.http import JsonResponse
+from rest_framework.response import Response
+
 # Create your views here.
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -74,13 +76,12 @@ def getUserProfile(request):
     serializer = UserSerializer(user, many=False)
     return JsonResponse(serializer.data)
 
-
 @api_view(['GET'])
 @permission_classes([IsAdminUser])
 def getUsers(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
-    return JsonResponse(serializer.data)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
