0f22325833cb950358e32fad95e9d229aae1b7a3
emcarlos <carlosemc12@gmail.com>
2025-06-22 22:05:32 -0300

    fix: improve order creation logic and error handling in addOrderItems function

--- base/views/order_views.py
+++ base/views/order_views.py
@@ -16,57 +16,59 @@ from base.serializers import OrderSerializer, ProductSerializer
 def addOrderItems(request):
     user = request.user
     data = request.data
+    try:
+        orderItems = data['orderItems']
 
-    orderItems = data['orderItems']
-
-    if orderItems and len(orderItems) == 0:
-        return JsonResponse({'detail': 'No Order Items'}, status=status.HTTP_400_BAD_REQUEST)
-    else:
-
-        # (1) Create order
-
-        order = Order.objects.create(
-            user=user,
-            paymentMethod=data['paymentMethod'],
-            taxPrice=data['taxPrice'],
-            shippingPrice=data['shippingPrice'],
-            totalPrice=data['totalPrice']
-        )
-
-        # (2) Create shipping address
-
-        shipping = ShippingAddress.objects.create(
-            order=order,
-            address=data['shippingAddress']['address'],
-            phone=data['shippingAddress']['phone'],
-            messageFrom=data['shippingAddress']['messageFrom'],
-            messageFor=data['shippingAddress']['messageFor'],
-            message=data['shippingAddress']['message'],
-            city=data['shippingAddress']['city'],
-            postalCode=data['shippingAddress']['postalCode'],
-            country=data['shippingAddress']['country'],
-        )
-
-        # (3) Create order items adn set order to orderItem relationship
-        for i in orderItems:
-            product = Product.objects.get(_id=i['product'])
-
-            item = OrderItem.objects.create(
-                product=product,
-                order=order,
-                name=product.name,
-                qty=i['qty'],
-                price=i['price'],
-                image=product.image.url,
+        if orderItems and len(orderItems) == 0:
+            return JsonResponse({'detail': 'No Order Items'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            # (1) Create order
+            order = Order.objects.create(
+                user=user,
+                paymentMethod=data['paymentMethod'],
+                taxPrice=data['taxPrice'],
+                shippingPrice=data['shippingPrice'],
+                totalPrice=data['totalPrice']
             )
 
-            # (4) Update stock
+            # (2) Create shipping address
+            shipping = ShippingAddress.objects.create(
+                order=order,
+                address=data['shippingAddress']['address'],
+                phone=data['shippingAddress']['phone'],
+                messageFrom=data['shippingAddress']['messageFrom'],
+                messageFor=data['shippingAddress']['messageFor'],
+                message=data['shippingAddress']['message'],
+                city=data['shippingAddress']['city'],
+                postalCode=data['shippingAddress']['postalCode'],
+                country=data['shippingAddress']['country'],
+            )
 
-            product.countInStock -= item.qty # type: ignore
-            product.save()
+            # (3) Create order items and set order to orderItem relationship
+            for i in orderItems:
+                product = Product.objects.get(_id=i['product'])
 
-        serializer = OrderSerializer(order, many=False)
-        return JsonResponse(serializer.data)
+                item = OrderItem.objects.create(
+                    product=product,
+                    order=order,
+                    name=product.name,
+                    qty=i['qty'],
+                    price=i['price'],
+                    image=product.image.url,
+                )
+
+                # (4) Update stock
+                product.countInStock -= item.qty  # type: ignore
+                product.save()
+
+            serializer = OrderSerializer(order, many=False)
+            return JsonResponse(serializer.data)
+    except Product.DoesNotExist:
+        return JsonResponse({'detail': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+    except KeyError as e:
+        return JsonResponse({'detail': f'Missing field: {str(e)}'}, status=status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        return JsonResponse({'detail': f'Error creating order: {str(e)}'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
