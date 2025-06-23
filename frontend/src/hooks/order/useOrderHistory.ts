b82401640d3c8560099861c3212eb877f937028d
emcarlos <carlosemc12@gmail.com>
2025-06-23 00:14:41 -0300

    fix: ensure order and user lists are only fetched for logged-in admin users

--- frontend/src/hooks/order/useOrderHistory.ts
+++ frontend/src/hooks/order/useOrderHistory.ts
@@ -1,13 +1,16 @@
 import { ordersApi } from "@/services/apiServices";
+import useGeneralStore from "@/store";
 import { useQuery } from "@tanstack/react-query";
 
 export const useOrderHistory = () => {
+  const { userLogged } = useGeneralStore();
   const {
     data: orderHistory = [],
     isLoading,
     error,
   } = useQuery({
     queryKey: ["orders"],
+    enabled: !!userLogged && userLogged?.isAdmin,
     queryFn: ordersApi.getOrderHistory,
     staleTime: 1000 * 60 * 60 * 24, // 24 hours
   });
--- frontend/src/hooks/user/useUsersList.ts
+++ frontend/src/hooks/user/useUsersList.ts
@@ -1,13 +1,16 @@
 import { userApi } from "@/services/apiServices";
+import useGeneralStore from "@/store";
 import { useQuery } from "@tanstack/react-query";
 
 export const useUsersList = () => {
+  const { userLogged } = useGeneralStore();
   const {
     data: usersList,
     isLoading: isLoadingUsersList,
     error: errorUsersList,
   } = useQuery({
     queryKey: ["listUsers"],
+    enabled: !!userLogged && userLogged?.isAdmin,
     queryFn: userApi.listUsers,
     staleTime: 1000 * 60 * 60 * 24, // 24 hours
   });
